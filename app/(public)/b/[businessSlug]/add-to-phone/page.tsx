a85457ca7fd324d3bd754c0de52656ae84ea374d
englertjjames32 <englertjjames@gmail.com>
2026-04-07 22:08:11 -0400

    Create page.tsx

---
+++ app/(public)/b/[businessSlug]/add-to-phone/page.tsx
@@ -0,0 +1,13 @@
+import { SectionCard } from "@/components/section-card";
+
+export default function AddToPhonePage() {
+  return (
+    <SectionCard title="Add to Phone">
+      <ol>
+        <li>Open this page in your mobile browser.</li>
+        <li>Tap the browser menu and choose "Add to Home Screen".</li>
+        <li>Save as OSPARA service card.</li>
+      </ol>
+    </SectionCard>
+  );
+}
