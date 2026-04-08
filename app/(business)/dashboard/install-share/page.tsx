f8cf19da670345d82896345ee7fe49ece72d1046
englertjjames32 <englertjjames@gmail.com>
2026-04-07 22:00:45 -0400

    Create page.tsx

---
+++ app/(business)/dashboard/install-share/page.tsx
@@ -0,0 +1,11 @@
+import { SectionCard } from "@/components/section-card";
+
+export default function BusinessInstallSharePage() {
+  return (
+    <SectionCard title="Install & Share">
+      <p>Share this public link with customers:</p>
+      <code>/b/demo-auto</code>
+      <p>Customers can add the page to their phone from the public Add to Phone page.</p>
+    </SectionCard>
+  );
+}
